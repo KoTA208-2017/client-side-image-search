eebedcb51aa906ae9508b511311eb48ee95c813a
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-13 22:40:55 +0700

    feat(capture-image): initial variable

--- app/src/main/java/com/example/image_search/CaptureImageActivity.java
+++ app/src/main/java/com/example/image_search/CaptureImageActivity.java
@@ -2,13 +2,56 @@ package com.example.image_search;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.ImageButton;
+
+import java.io.File;
 
 public class CaptureImageActivity extends AppCompatActivity {
+
+    ImageButton captureBtn;
+    TextureView textureView;
+    String imagePath;
+    private static final SparseIntArray ORIENTATION = new SparseIntArray();
+
+    static {
+        ORIENTATION.append(Surface.ROTATION_0, 90);
+        ORIENTATION.append(Surface.ROTATION_90, 0);
+        ORIENTATION.append(Surface.ROTATION_180, 270);
+        ORIENTATION.append(Surface.ROTATION_270, 180);
+    }
+
+    private String cameraId;
+    CameraDevice cameraDevice;
+    CameraCaptureSession cameraCaptureSession;
+    CaptureRequest captureRequest;
+    CaptureRequest.Builder captureRequestBuilder;
+
+    private Size imageDimensions;
+    private ImageReader imageReader;
+    private File file;
+    Handler nBackgroundHandler;
+    HandlerThread nBackgroundThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture_image);
+        
+        textureView = findViewById(R.id.textureView);
+        captureBtn = findViewById(R.id.captureBtn);
+        textureView.setSurfaceTextureListener(textureListener);
+
+        
     }
 }
