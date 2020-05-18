4c9ee24eda996c3216ef68bca3f7c8129add8403
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-18 09:04:31 +0700

    feat(preview-image): get string from intent

--- app/src/main/java/com/example/image_search/PreviewImageActivity.java
+++ app/src/main/java/com/example/image_search/PreviewImageActivity.java
@@ -2,13 +2,19 @@ package com.example.image_search;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PreviewImageActivity extends AppCompatActivity {
+
+    String imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_image);
+
+        Intent intent = getIntent();
+        imagePath = intent.getStringExtra("IMAGE_PATH");
     }
 }
