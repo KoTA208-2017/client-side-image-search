8891ea80f12b728d62190c4f1fb8660544caa49d
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-18 07:27:12 +0700

    feat(crop-image): add layouting asset

--- app/src/main/java/com/example/image_search/CropImageActivity.java
+++ app/src/main/java/com/example/image_search/CropImageActivity.java
@@ -2,10 +2,12 @@ package com.example.image_search;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class CropImageActivity extends AppCompatActivity {
-
+    Toolbar mTopToolbar;
     String imagePath;
 
     @Override
@@ -13,7 +15,11 @@ public class CropImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop_image);
 
+        mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+
         Intent intent = getIntent();
         imagePath = intent.getStringExtra("IMAGE_PATH");
+
     }
 }
