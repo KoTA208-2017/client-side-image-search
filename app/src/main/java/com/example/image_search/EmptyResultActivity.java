a80d8a07a2ad639e202a3ed7be93fc060f68e4d2
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-28 13:50:30 +0700

    feat(empty-result) add back button event handler

--- app/src/main/java/com/example/image_search/EmptyResultActivity.java
+++ app/src/main/java/com/example/image_search/EmptyResultActivity.java
@@ -2,13 +2,27 @@ package com.example.image_search;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EmptyResultActivity extends AppCompatActivity {
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty_result);
+
+        backBtn = findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent mIntent = new Intent(EmptyResultActivity.this, CaptureImageActivity.class);
+            startActivity(mIntent);
+            }
+        });
     }
 }
