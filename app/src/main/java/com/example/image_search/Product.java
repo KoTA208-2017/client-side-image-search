a1b236f4cb6195602d281e89b1d1742b3d699e52
refdinal22 <refdinal22@gmail.com>
2020-06-04 13:55:56 +0700

    feat(search-result): adjust product class

--- app/src/main/java/com/example/image_search/Product.java
+++ app/src/main/java/com/example/image_search/Product.java
@@ -8,28 +8,45 @@ public  class Product {
     @Expose
     private int id;
 
+    @SerializedName("name")
     @Expose
     private String name;
 
+    @SerializedName("siteName")
     @Expose
     private String siteName;
 
+    @SerializedName("url")
     @Expose
     private String url;
 
+    @SerializedName("price")
     @Expose
     private int price;
 
     @Expose
     private String image;
 
-    public Product(int id, String name, String siteName, String url, int price, String image) {
+    @SerializedName("imageUrl")
+    @Expose
+    private String imageUrl;
+
+    public Product(int id, String name, String siteName, String url, int price, String image, String imageUrl) {
         this.id = id;
         this.name = name;
         this.siteName = siteName;
         this.url = url;
         this.price = price;
         this.image = image;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
     public int getId() {
